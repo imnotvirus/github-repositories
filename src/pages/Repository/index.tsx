a8dee7321624cb6205996dfb4466e1e79f9f1bea
Luiz Claudio <lcluizclaudio96@gmail.com>
2021-06-18 18:03:20 -0300

    styled header

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router';
-
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+import { Header } from './styles';
+import logoImg from '../../assets/GitHub-logo.svg';
 interface RepositoryParams {
     repository: string;
 }
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
-    return <h1>Repository:{params.repository}</h1>;
+    return (
+        <Header>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Link to="/">
+                <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+        </Header>
+    );
 };
 
 export default Repository;
