343d703ca73507959de54e0c6e9879402e812c89
Luiz Claudio <lcluizclaudio96@gmail.com>
2021-06-22 10:31:50 -0300

    listando detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Header, RepositoryInfo, Issues } from './styles';
 import logoImg from '../../assets/GitHub-logo.svg';
+import api from '../../services/api';
 interface RepositoryParams {
     repository: string;
 }
+interface RepositoryInterface {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+interface IssueInterface {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    };
+}
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
+    const [repository, setRepository] = useState<RepositoryInterface | null>(
+        null,
+    );
+    const [issues, setIssues] = useState<IssueInterface[]>([]);
+    useEffect(() => {
+        api.get(`repos/${params.repository}`).then((response) => {
+            setRepository(response.data);
+        });
+        api.get(`repos/${params.repository}/issues`).then((response) => {
+            setIssues(response.data);
+        });
+    }, [params.repository]);
+    /*async function loadData(): Promise<void> {
+    const [repository, issues] = await Promise.all([
+        api.get(`repos/${params.repository}`),
+        api.get(`repos/${params.repository}/issues`)
+    ]);
+}*/
     return (
         <>
             <Header>
@@ -17,47 +55,45 @@ const Repository: React.FC = () => {
                     Voltar
                 </Link>
             </Header>
-            <RepositoryInfo>
-                <header>
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/289229274?v=4"
-                        alt="RcokeactSeat"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Itaque, dolorum delectus asperiores impedit
-                            cumque corporis odio officiis voluptates inventore
-                            odit totam aliquid eos soluta eligendi ipsum
-                            blanditiis reiciendis labore consectetur?
-                        </p>
-                    </div>
-                </header>
-                <ul>
-                    <li>
-                        <strong>1808</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>48</strong>
-                        <span>forks</span>
-                    </li>
-                    <li>
-                        <strong>67</strong>
-                        <span>issues abertas</span>
-                    </li>
-                </ul>
-            </RepositoryInfo>
+            {repository && (
+                <RepositoryInfo>
+                    <header>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>issues abertas</span>
+                        </li>
+                    </ul>
+                </RepositoryInfo>
+            )}
 
             <Issues>
-                <Link to="repository/repository.full_name">
-                    <div>
-                        <strong>"repository.full_name"</strong>
-                        <p>"repository.description"</p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </Link>
+                {issues.map((issue) => (
+                    <a key={issue.id} href={issue.html_url}>
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Issues>
         </>
     );
